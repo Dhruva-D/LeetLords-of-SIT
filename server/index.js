8c6cfec94ab7674c1be33fca46a46df7ea5498ce
Dhruva-D <dhruvad575@gmail.com>
2025-01-06 00:54:22 +0530

    Phase1Deployment

--- server/index.js
+++ server/index.js
@@ -1,24 +1,22 @@
-const express = require('express')
-const { connectToMongoDB } = require('./connect')
-const userRoute = require('./routes/user')
-const leaderboardRoute = require('./routes/leaderboard')
+const express = require('express');
+const { connectToMongoDB } = require('./connect');
+const userRoute = require('./routes/user');
+const leaderboardRoute = require('./routes/leaderboard');
 const path = require('path');
 
+const app = express();
+const PORT = 5000;
 
-const app = express()
-const PORT = 5000
+connectToMongoDB("mongodb+srv://Dhruva:8UyTM-vwSQaqAtP@cluster0.r5dk3.mongodb.net/leetlords?retryWrites=true&w=majority&appName=Cluster0")
+.then( () => console.log("Connected to MongoDB")).catch((err) => console.log('error', err))
 
-connectToMongoDB("mongodb://localhost:27017/Leet-Lords")
-.then(() => console.log("MongoDB connected"))
-
-app.set("view engine" , "ejs")
-app.set("views" , path.resolve("./views"))
-
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/register", userRoute)
-app.use("/", leaderboardRoute)
+app.use("/register", userRoute);
+app.use("/", leaderboardRoute);
 
-app.listen(PORT, () => console.log(`Server Running on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
